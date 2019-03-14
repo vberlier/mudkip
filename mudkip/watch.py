b984266ac60e487bcdb10143efb0efa7d3dfdc42
Valentin Berlier <berlier.v@gmail.com>
2019-03-14 15:52:03 -0400

    Debounce directory watcher

--- mudkip/watch.py
+++ mudkip/watch.py
@@ -1,16 +1,27 @@
 from queue import Queue
+from threading import Timer
 
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 
 
 class QueueHandler(PatternMatchingEventHandler):
+    debounce = 0.001
+
     def __init__(self, queue, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.queue = queue
+        self.timer = None
 
     def on_any_event(self, event):
+        if self.timer is not None:
+            self.timer.cancel()
+        self.timer = Timer(self.debounce, self.debounced_callback, args=[event])
+        self.timer.start()
+
+    def debounced_callback(self, event):
         self.queue.put(event)
+        self.timer = None
 
 
 def watch_directory(
